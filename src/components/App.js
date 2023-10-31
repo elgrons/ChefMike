f32a1709b574929222f3ed99828e44ccce32181a
Eliot Gronstal <eliot.lauren@gmail.com>
2023-10-31 13:58:24 -0700

    add Welcome.js static component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import { NavBar } from "./NavBar";
+import { Header } from "./Header";
+import { Welcome } from "./Welcome";
+
 
 function App() {
   return (
     <>
     <div className="App">
+    <Header />
+    <Welcome />
     </div>
     </>
   );
