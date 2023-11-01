010b08673b7c619623986735c483b168d48ffad9
Eliot Gronstal <eliot.lauren@gmail.com>
2023-11-01 11:32:16 -0700

    update App.js and Header.js with React Router.

--- src/components/App.js
+++ src/components/App.js
@@ -8,22 +8,26 @@ import { BookingInfo } from "./BookingInfo";
 import { MediaFeed } from "./MediaFeed";
 import { Contact } from "./Contact";
 import { Footer } from "./Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+
   return (
-    <>
+    <Router>
     <div className="App">
     <Header />
-    <Welcome />
-    <Why />
-    <AboutTheChef />
-    <MenuControl />
-    <BookingInfo />
-    <MediaFeed />
-    <Contact />
+    <Routes>
+      <Route exact path="/" element={Welcome} />
+      <Route exact path="/why" element={Why} />
+      <Route exact path="/about" element={AboutTheChef} />
+      <Route exact path="/menu" element={MenuControl} />
+      <Route exact path="/booking" element={BookingInfo} />
+      <Route exact path="/media" element={MediaFeed} />
+      <Route exact path="/contact" element={Contact} />
+    </Routes>
     <Footer />
     </div>
-    </>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,38 @@
 import './App.css';
-import * as React from 'react';
+import React from "react";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/Menu';
-import Button from '@mui/material/Button';
+import { makeStyles } from '@material-ui/core';
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  navlinks: {
+    marginLeft: theme.spacing(10),
+    display: "flex",
+  },
+logo: {
+    flexGrow: "1",
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: theme.spacing(20),
+    "&:hover": {
+      color: "yellow",
+      borderBottom: "1px solid white",
+    },
+  },
+}));
 
 export const Header = () => {
+  const classes = useStyles();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -22,10 +46,32 @@ export const Header = () => {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Typography variant="h4" className={classes.logo}>
+          Chef Mike
+        </Typography>
+          <div className={classes.navlinks}>
+            <Link to="/" className={classes.link}>
+              Home
+            </Link>
+            <Link to="/why" className={classes.link}>
+              The Why
+            </Link>
+            <Link to="/about" className={classes.link}>
+              About
+            </Link>
+            <Link to="/menu" className={classes.link}>
+              Sample Menus
+            </Link>
+            <Link to="/booking" className={classes.link}>
+              Booking Info
+            </Link>
+            <Link to="/media" className={classes.link}>
+            Media & Testimonials
+            </Link>
+            <Link to="/contact" className={classes.link}>
+            Contact
+            </Link>
+            </div>
         </Toolbar>
       </AppBar>
     </Box>
