b79700c7e23d8d350ce1333de78b1f18856e7ace
Eliot Gronstal <eliot.lauren@gmail.com>
2023-12-28 16:12:05 -0800

    adjust css stylings in footer.scss.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,18 @@
-import  { React } from "react";
-import './Footer.scss';
+import { React } from "react";
+import "./Footer.scss";
 import InstaLink from "../../assets/img/ig-icon.svg";
-
 
 export const Footer = () => {
   return (
     <>
-    <p>Chef Mike Footer</p>
-    <div className="social-icon">
-    <a href="https://www.instagram.com/advntrsekr/"><img src={InstaLink} alt="Instagram nav icon" /></a>
-    </div>
+      <div className="footer-section">
+        <p>Chef Mike Footer</p>
+        <div className="social-icon">
+          <a href="https://www.instagram.com/advntrsekr/">
+            <img src={InstaLink} alt="Instagram nav icon" />
+          </a>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
