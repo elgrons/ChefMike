bfae7649bf531c02d7064f4ce076a244b969e8b5
Eliot Gronstal <eliot.lauren@gmail.com>
2024-01-10 15:49:30 -0800

    install tailwind css for additional stylings. move picture of chef from welcome to about.

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,12 +1,12 @@
 import  { React } from "react";
-import welcomeImg from "../../assets/img/Chef-Mike.png";
+// import welcomeImg from "../../assets/img/Chef-Mike.png";
 import './Welcome.scss';
 
 export const Welcome = () => {
   return (
     <>
     <h1>Your Private Chef: Mike</h1>
-    <img src={welcomeImg} alt="Welcome Img of Chef Mike"/>
+    {/* <img src={welcomeImg} alt="Welcome Img of Chef Mike"/> */}
     </>
   )
 }
