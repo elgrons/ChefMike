ae392539d531aac96259b5eb1f573a978a4b3884
Eliot Gronstal <eliot.lauren@gmail.com>
2024-04-17 16:05:50 -0700

    WIP: adding instagram embed.

--- src/components/BookingInfo/BookingInfo.js
+++ src/components/BookingInfo/BookingInfo.js
@@ -1,18 +1,38 @@
-import  { React } from "react";
-import './BookingInfo.scss';
-
+import { React } from "react";
+import "./BookingInfo.scss";
+import { Grid } from "@mui/material";
 
 export const BookingInfo = () => {
   return (
     <>
-    <h1>Booking with me includes: dinner for up to 16, specialized menus we agree on, I cook and clean.</h1>
-    <h1>I can coordinate bartender/server upon request.</h1>
-    <h1>Anywhere in the PNW, but specializing in the Coast, Mt. Hood, Portland-metro.</h1>
-    <h1>Will discuss further travel and expenses per individual event request.</h1>
-    <h1>Minimum cost of $ per person.</h1>
-    <h1>Minimum group size of x people.</h1>
-    <h1>Sustainably sourced fresh food from the PNW (including my garden!).</h1>
-    <h1>Call or Text Directly to Set Up Booking.</h1>
+      <Grid container spacing={3}>
+        <Grid item m={6}>
+          <h1>
+            Booking with me includes:
+            <ul>
+              <li>Dinner for up to 16</li>
+              <li>Specialized menus coordinated in advance</li>
+              <li>I cook and clean!</li>
+            </ul>
+          </h1>
+          <br></br>
+          <h1>Bartender/server coordinated upon request.</h1>
+          <br></br>
+          <h1>Location:</h1>
+          <h1>
+            Specializing in the Oregon Coast, Mt. Hood, and Portland-metro areas, but will travel anywhere in the PNW.
+          </h1>
+          <h1>
+            Can discuss further travel and expenses per individual event request.
+          </h1>
+          <br></br>
+          <h1>Cost:</h1>
+          <h1>Minimum cost of $ per person.</h1>
+          <h1>Minimum group size of x people.</h1>
+          <br></br>
+          <h1>Call or Text directly to book.</h1>
+        </Grid>
+      </Grid>
     </>
-  )
-}
+  );
+};
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,12 +1,28 @@
 import  { React } from "react";
 // import welcomeImg from "../../assets/img/Chef-Mike.png";
 import './Welcome.scss';
+import InstagramEmbed from 'react-instagram-embed';
+
 
 export const Welcome = () => {
   return (
     <>
     <h1>Your Private Chef: Mike</h1>
-    <img src="https://www.instagram.com/p/CyO8wbGSkX0/" />
+    <InstagramEmbed
+  url='https://www.instagram.com/p/CrqybT5Jx3u/'
+  clientAccessToken=''
+  maxWidth={320}
+  hideCaption={false}
+  containerTagName='div'
+  protocol=''
+  injectScript
+  onLoading={() => {}}
+  onSuccess={() => {}}
+  onAfterRender={() => {}}
+  onFailure={() => {}}
+/>
+
+
     </>
   )
 }
